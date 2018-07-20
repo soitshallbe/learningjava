67747142bde4e6867ecbf0fa94d3cb6242a1db99
LFo <luana.forloni@gmail.com>
2018-07-20 11:48:34 +0200

    Test commit

---
+++ src/learningjava/Core.java
@@ -0,0 +1,10 @@
+package learningjava;
+
+public class Core {
+	
+	public static void main (String[] argc) {
+		System.out.println("*** Java Core Language Tutorial");
+		System.out.println("Test Commit");
+	}
+
+}
